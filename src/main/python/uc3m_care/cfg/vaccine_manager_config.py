455e8960770c36ec6ba739fd868d1b97b4d3e4d7
valentinacjim <98902987+valentinacjim@users.noreply.github.com>
2022-05-19 13:11:27 +0200

    Add files via upload

---
+++ src/main/python/uc3m_care/cfg/vaccine_manager_config.py
@@ -0,0 +1,4 @@
+"""Global constants for finding the path"""
+from pathlib import Path
+JSON_FILES_PATH = str(Path.home()) + "/PycharmProjects/2022_EG4_BASECODE_2/src/JsonFiles/"
+JSON_FILES_RF2_PATH = JSON_FILES_PATH + "/RF2/"
